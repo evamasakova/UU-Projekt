e3a19393de03c7bec65863236a95cd0bcc6f7630
Andrii Krailo <andriikrailo@gmx.de>
2025-11-06 18:31:22 +0100

    merge

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,11 @@
-// Minimal placeholder API client
-export const apiClient = {
-  async get(path) {
-    throw new Error('Not implemented: GET ' + path)
-  },
-}
+import axios from "axios";
 
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+});
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
