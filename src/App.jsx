48027e68b08a2dfa0f7938a554309ac88d5ce854
lukokokajn <lukas.novy.1010@gmail.com>
2025-11-10 20:41:55 +0100

    Delete Button in JS

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import DeleteButton from "./components/DeleteButton.jsx";
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,11 +29,13 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+        <DeleteButton onClick={() => alert("Delete!")} />
+        <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
   )
 }
+
 
 export default App
