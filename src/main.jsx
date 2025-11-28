c82901ff3b4fa94ba5b0c255f88ff741e19e1a7a
Davey1-2 <72246250+Davey1-2@users.noreply.github.com>
2025-11-28 12:27:46 +0100

    Revert "auth"

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppRouter from "./AppRouter.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <AppRouter />
-    </AuthProvider>
+    <AppRouter />
   </StrictMode>,
 );
