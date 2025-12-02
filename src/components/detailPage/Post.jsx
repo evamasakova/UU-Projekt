c66fcdddeb9137309ce5292a7c318f9173d2e9c3
Simon Petr Stokuc <simon.p.stokuc@gmail.com>
2025-12-02 19:28:16 +0100

    feat: optimized loading data, added postLog section

---
+++ src/components/detailPage/Post.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Post({title, content, date}) {
+    return (
+        <div className="m-4">
+            <div>
+                <p className="font-medium">{title}</p>
+                <p className="text-xs text-gray-500">{date.split("T")[0]}</p>
+            </div>
+            <p className="text-sm mt-1">
+                {content}
+            </p>
+        </div>
+    )
+}
