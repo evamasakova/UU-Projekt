bf01b4fb04bbe7c20226898556178d9810bfb7f3
Simon Petr Stokuc <simon.p.stokuc@gmail.com>
2025-12-23 16:55:18 +0100

    fix, feat: filtering campaigns, added current amount

--- src/components/CampaignList.jsx
+++ src/components/CampaignList.jsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState, useRef} from "react";
+import React, { useEffect, useState, useRef } from "react";
 import FundingCard from "./cards/CampaignCard.jsx";
-import {useApi} from "../api/apiClient.js";
+import { useApi } from "../api/apiClient.js";
 
 export default function CampaignList() {
     const fetchedRef = useRef(false);
@@ -14,8 +14,8 @@ export default function CampaignList() {
         if (fetchedRef.current) return;
         fetchedRef.current = true;
 
-        const resCat = await api("/categories", {method: "GET"});
-        const resCam = await api("/projects", {method: "GET"});
+        const resCat = await api("/categories", { method: "GET" });
+        const resCam = await api("/projects", { method: "GET" });
 
         setCategories(resCat);
         setCampaigns(resCam);
@@ -31,43 +31,49 @@ export default function CampaignList() {
             : campaigns.filter(
                 (c) => c.categoryId === selectedCategory
             );
+
     return (
         <div className="w-full lg:mx-8">
             <div className="flex items-center mb-6">
-                <span className="text-2xl font-semibold mr-6">Browse Campaigns</span>
+        <span className="text-2xl font-semibold mr-6">
+          Browse Campaigns
+        </span>
+
                 <select
                     className="
-                        border border-gray-300
-                        rounded-lg
-                        px-4 py-2
-                        text-gray-700
-                        shadow-sm
-                        focus:outline-none
-                        focus:ring-2 focus:ring-purple-500
-                    "
+            border border-gray-300
+            rounded-lg
+            px-4 py-2
+            text-gray-700
+            shadow-sm
+            focus:outline-none
+            focus:ring-2 focus:ring-purple-500
+          "
                     value={selectedCategory}
                     onChange={(e) => setSelectedCategory(e.target.value)}
                 >
                     <option value="All">All Categories</option>
-                    {categories.map((cat, index) => (
-                        <option key={index} value={cat.name}>
+
+                    {categories.map((cat) => (
+                        <option key={cat._id} value={cat._id}>
                             {cat.name}
                         </option>
                     ))}
                 </select>
             </div>
+
             <div
                 className="
-                grid
-                grid-cols-1
-                sm:grid-cols-2
-                lg:grid-cols-3
-                gap-4
-            "
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          lg:grid-cols-3
+          gap-4
+        "
             >
-                {filteredCampaigns.map((c, index) => (
+                {filteredCampaigns.map((c) => (
                     <FundingCard
-                        key={index}
+                        key={c._id}
                         id={c._id}
                         title={c.name}
                         description={c.description}
